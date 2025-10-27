ec69b497ab36a1971977f703c2a7a64b6307a301
thismarcoantonio <thismarcoantonio@gmail.com>
2025-10-26 18:30:04 -0700

    Use new components in Login page and LoginForm component

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useState, FormEvent } from "react";
 import { useFormStatus } from "react-dom";
+import Card from "@/app/components/Card";
+import TextField from "@/app/components/TextField";
+import Button from "@/app/components/Button";
 
 export default function LoginForm({
   action,
@@ -17,19 +20,29 @@ export default function LoginForm({
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-      />
-      <input
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        type="password"
-        placeholder="Senha"
-      />
-      <button disabled={pending}>Entrar</button>
-    </form>
+    <Card className="mx-auto max-w-md text-center">
+      <h1 className="text-lg font-bold">Sofa sellers</h1>
+      <p className="mb-6 text-sm text-gray-500">
+        Faça login com as suas credenciais
+      </p>
+      <form onSubmit={onSubmit}>
+        <TextField
+          label="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="admin@example.com"
+        />
+        <TextField
+          label="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="••••••••"
+        />
+        <Button className="w-full" type="submit" disabled={pending}>
+          Entrar
+        </Button>
+      </form>
+    </Card>
   );
 }

a393c590c21da740534af33b3bd8d3dd55a6fc49
leuzaoo <leuzaoocontato@gmail.com>
2025-11-03 23:13:43 +0000

    feat: updated font style of app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
-// import SupabaseListener from "./components/supabase-listener";
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Manrope } from "next/font/google";
+import type { Metadata } from "next";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const manrope = Manrope({
+  variable: "--font-manrope",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${manrope.className} antialiased`}>
         {/* <SupabaseListener /> */}
         {children}
       </body>

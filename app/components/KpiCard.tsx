c7fcdf2fd00f8f2a6e05d92903790b1f0dc570a4
leuzaoo <dev.lcostaoliveira@gmail.com>
2025-11-13 21:22:55 +0000

    fix: minimum width added to KPI card component

--- app/components/KpiCard.tsx
+++ app/components/KpiCard.tsx
@@ -3,10 +3,10 @@ export default function KpiCard({
   value,
 }: {
   title: string;
-  value: string | number;
+  value: string | number | undefined;
 }) {
   return (
-    <div className="bg-pattern-100 text-darker border-pattern-200 rounded-lg border p-4">
+    <div className="bg-pattern-100 text-darker border-pattern-200 min-w-48 rounded-lg border p-4">
       <div className="text">{title}</div>
       <div className="mt-1 text-2xl font-bold">{value}</div>
     </div>

48ae9b47483a24fbac71068babe96d4189d7e0ed
leuzaoo <dev.lcostaoliveira@gmail.com>
2025-11-18 17:03:55 +0000

    feat: updated customers types

--- types/CustomersTableRow.tsx
+++ types/CustomersTableRow.tsx
@@ -1,8 +1,19 @@
 export type CustomersTableRow = {
   id: string;
   name: string;
-  document: string;
-  postal_code: number | string;
+  document: string | null;
+  phone: string;
+  created_by: string;
+  district: string;
+  street: string;
+  number: string;
+  complement: string | null;
+  postal_code: string;
+  city: string;
   state: string;
   created_at: string;
+  creator?: {
+    id: string;
+    name: string;
+  } | null;
 };

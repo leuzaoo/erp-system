88edc5e75e2b65ea5d6265b5b172ce45521a55f4
leuzaoo <dev.lcostaoliveira@gmail.com>
2025-11-18 17:04:21 +0000

    feat: layout of customer view page has been updated

--- app/(private)/customers/[id]/page.tsx
+++ app/(private)/customers/[id]/page.tsx
@@ -1,6 +1,10 @@
 import { requireRole } from "@/utils/auth/requireRole";
 import { supabaseRSC } from "@/utils/supabase/rsc";
 
+import { CustomersTableRow } from "@/types/CustomersTableRow";
+
+import Card from "@/app/components/Card";
+
 export default async function CustomerViewPage({
   params,
 }: {
@@ -17,19 +21,125 @@ export default async function CustomerViewPage({
   const { data: customer, error } = await supabase
     .from("customers")
     .select(
-      "id, name, document, phone, created_by, district, street, number, complement, postal_code, city,  state, created_at",
+      `
+      id, name, document, phone, created_by,
+      district, street, number, complement, postal_code, city, state, created_at,
+      creator:profiles!customers_created_by_fkey (
+        id, name
+      )
+    `,
     )
     .eq("id", id)
-    .single();
+    .single<CustomersTableRow>();
 
-  if (error) {
-    return <pre className="text-red-600">Erro: {error.message}</pre>;
+  if (error || !customer) {
+    return (
+      <pre className="text-red-600">
+        Erro ao carregar cliente: {error?.message ?? "Não encontrado."}
+      </pre>
+    );
   }
 
+  const shortId = customer.id.slice(0, 5).toUpperCase();
+  const createdAt = customer.created_at
+    ? new Date(customer.created_at).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+      })
+    : "—";
+
   return (
-    <>
-      <h1>Página do cliente: {customer?.name}</h1>
-      <h1>{customer.phone}</h1>
-    </>
+    <div className="space-y-6">
+      <section className="flex items-center justify-between">
+        <div>
+          <h1 className="text-2xl font-bold">{customer.name}</h1>
+          <p className="mt-1 text-sm">
+            ID do cliente: <span className="font-medium">#{shortId}</span>
+          </p>
+        </div>
+
+        <div className="text-right text-sm">
+          <p>Cadastrado em</p>
+          <p className="font-medium text-neutral-800">{createdAt}</p>
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-4 lg:flex-row">
+        <Card>
+          <h2 className="mb-3 text-lg font-bold">Dados do cliente</h2>
+          <dl className="grid gap-3 md:grid-cols-2">
+            <div>
+              <dt className="text-xs uppercase">Nome completo</dt>
+              <dd className="text-sm font-semibold">{customer.name}</dd>
+            </div>
+
+            <div>
+              <dt className="text-xs uppercase">Documento</dt>
+              <dd className="text-sm font-semibold">
+                {customer.document || "—"}
+              </dd>
+            </div>
+
+            <div>
+              <dt className="text-xs uppercase">Telefone</dt>
+              <dd className="text-sm font-semibold">{customer.phone || "—"}</dd>
+            </div>
+
+            <div>
+              <dt className="text-xs uppercase">Criado por (ID do usuário)</dt>
+              <dd className="text-sm font-semibold">
+                {customer.creator?.name ?? "—"}
+              </dd>
+            </div>
+          </dl>
+        </Card>
+
+        <Card>
+          <h2 className="mb-3 text-lg font-semibold">Endereço</h2>
+
+          <div className="space-y-4 text-sm">
+            <p>
+              Rua: <br />
+              <span className="font-semibold">
+                {customer.street && customer.number
+                  ? `${customer.street}, ${customer.number}`
+                  : "Endereço não informado"}
+              </span>
+            </p>
+
+            <p>
+              Bairro: <br />{" "}
+              <span className="font-semibold">{customer.district || "—"}</span>
+            </p>
+
+            <p>
+              Cidade: <br />
+              <span className="font-semibold">
+                {customer.city && customer.state
+                  ? `${customer.city} - ${customer.state}`
+                  : "Cidade/Estado não informados"}
+              </span>
+            </p>
+
+            <p>
+              CEP: <br />
+              <span className="font-semibold">
+                {customer.postal_code
+                  ? `${customer.postal_code}`
+                  : "CEP não informado"}
+              </span>
+            </p>
+
+            <p>
+              Complemento: <br />
+              <span className="font-semibold">
+                {customer.complement ? `${customer.complement}` : ""}
+              </span>
+            </p>
+          </div>
+        </Card>
+      </section>
+    </div>
   );
 }

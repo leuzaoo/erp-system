c4388d76facc146a262ca418ade63a38c1cce15e
leuzaoo <dev.lcostaoliveira@gmail.com>
2025-11-20 23:29:03 +0000

    feat: updated error message

--- app/components/forms/NewCustomerForm.tsx
+++ app/components/forms/NewCustomerForm.tsx
@@ -42,9 +42,7 @@ const NewCustomerForm = ({ closeModal, onCreated }: NewCustomerFormProps) => {
       !number ||
       !postal_code
     ) {
-      setError(
-        "Por favor, preencha todos os campos obrigatórios (*) antes de salvar.",
-      );
+      setError("Preencha todos os campos obrigatórios (*) antes de salvar.");
       return;
     }
 
@@ -85,12 +83,6 @@ const NewCustomerForm = ({ closeModal, onCreated }: NewCustomerFormProps) => {
           />
         </div>
 
-        {error && (
-          <div className="mb-3 rounded-md border border-red-800 bg-red-950/40 px-3 py-2 text-xs text-red-300">
-            {error}
-          </div>
-        )}
-
         <form onSubmit={handleSubmit} className="space-y-3">
           <Input name="name" label="Nome*" required autoFocus />
           <Input name="document" label="Documento" />
@@ -129,6 +121,11 @@ const NewCustomerForm = ({ closeModal, onCreated }: NewCustomerFormProps) => {
               )}
             </Button>
           </div>
+          {error && (
+            <div className="mb-3 rounded-md border border-red-800 bg-red-200 px-3 py-2 text-sm font-semibold text-red-600">
+              {error}
+            </div>
+          )}
         </form>
       </div>
     </div>
